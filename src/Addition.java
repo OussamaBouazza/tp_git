8cda42cc0c8e3e25725b5d5696ac2d353f19c076
Nicolas Boisseau <nicolasboisseau7@gmail.com>
2022-05-20 16:59:30 +0200

    Test addition

--- src/Addition.java
+++ src/Addition.java
@@ -3,4 +3,14 @@ public class Addition implements Calcul {
     public int calculer(int a, int b) {
         return a+b;
     }
+
+    public static void main(String[] args) {
+        Addition a = new Addition();
+        System.out.println(a.calculer(4,7));
+        System.out.println(a.calculer(9,14));
+        System.out.println(a.calculer(6,6));
+        System.out.println(a.calculer(8,8));
+        System.out.println(a.calculer(15,15));
+
+    }
 }
